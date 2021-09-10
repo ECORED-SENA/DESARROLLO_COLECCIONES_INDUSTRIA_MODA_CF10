05d8d3fc53039d634c8fe16e243ebba5e025b89c
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2021-09-10 16:25:04 -0500

    ajustes

--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -6,7 +6,199 @@
     .titulo-principal
       .titulo-principal__numero
         span 7
-      h1  SIN TITULO
+      h1  Tejidos para drapeados
+
+    .row.justify-content-center
+      .col-lg-2           
+        div.BorTit.mb-2
+          p.mb-0 <b>Figura</b>: Tejido.
+    figure.mb-5
+      img(src='@/assets/curso/tema7/img01.png')
+
+    p.mb-5 Los tejidos para drapeado pueden ser en plano o de punto según el efecto que se quiera dar al diseño. Dentro de sus características están Los tejidos de textura suave, permeabilidad, elasticidad, firmeza y grosor ya que de esto depende la caída o fluidez que pueda dar en las formas sobre el gabarit. Si el efecto a lograr es volumen con cuerpo y sostenerse se necesitarán tejidos como Tafetán y Tafeta, Shantung, sedas gruesas, pero si el efecto es suelto, con caída y ligero se recurre a texturas más finas como Satín, Sedas delgadas, Organza, Chiffon, y Tul. A continuación, se mencionan algunas de ellas desde su estructura y usos.
+
+    div.tema7-div1.mb-4.py-5(:style="{'background-image': `url(${require('@/assets/curso/tema7/img02.svg')})`}")
+      SlyderA
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Raso</b>
+                    td También es llamado Satén. Tela de seda o con urdimbre de seda y algodón mercerizado y trama de otros materiales, de superficie lisa y lustrosa y textura suave. Muy usado antiguamente en decoración.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img03.png')    
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Seda</b>
+                    td Proviene del capullo del gusano de seda. La seda es la fibra natural más fina, resistente y suave, su brillo le proporciona una textura lujosa. Tiene un buen poder aislante, muy elástico, fuerte y ligero, absorbente, tacto crujiente, delicado al frote y al sudor.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img04.png')    
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Visillo</b>
+                    td Cualquier tela fina o traslúcida, blanca o de color "blanco roto". Tiene buena caída y es suave al tacto.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img05.png')    
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Organza</b>
+                    td Tela aprestada, algo traslúcida y con textura, está tejida con un hilo de seda enrollado en la urdimbre.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img06.png')    
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Chiffon</b>
+                    td Tejido sedoso diáfano, fino, con una superficie granulada; está hecho en tela de lino con hebras de seda natural o fibra sintética en torsión en S y en Z. Se usa en la producción de vestidos ligeros, chales y bufandas.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img07.png')    
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Shantung</b>
+                    td Suntuosa tela de seda tejida de un solo color, con textura rugosa. En la actualidad se imita con fibras sintéticas y se utiliza para accesorios lujosos. Sirve para tapizar muebles cuando es espeso y resistente, pero también se emplea para confeccionar visillos. Suele llevar mezcla de Acetato o de Nailon.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img08.png')    
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Tul Bordado</b>
+                    td Apareció mediados de siglo XVIII en Tullé (Francia) con la fabricación manual de tules, derivados del encaje de bolillos. Hoy en día se obtienen unos telares especiales para Tul (bobinet) y en telar Rashel y Ketten, existiendo diversas modalidades. Se utilizan para corsetería, adornos, velos y mantillas, entre otros.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img09.png')    
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Raso de seda</b>
+                    td Del latín “seta” (seda). Tela de seda, o con urdimbre de seda o algodón mercerizado y con trama de otros materiales de superficie lisa y lustrosa. Muy usado antiguamente para vestidos de señora y decoración, hoy en día muy empleado en forraría y vestidos de novia.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img10.png')    
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Brocado</b>
+                    td Del francés “brochè” que significa husillo, donde iban arrollados los hilos de efecto. En su origen eran tejidos de seda y oro, adornados con flores y figuras. Actualmente se llama así a la tela mezclada con metal precioso. Se emplea para ornamentos de iglesia y tejidos suntuarios en vestido de novia.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img11.png')  
+                        figcaption  Nota. Tomado de https://www.shutterstock.com/es/image-photo/elegant-background-image-luxurious-white-satin-103118426
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Tafetán</b>
+                    td Tejido de seda o rayón de poca densidad, ligamento tafetán. Se usa para vestidos o prendas de adorno. Se fabrica un tejido análogo o algún parecido con algodón peinado y, algunas veces mercerizado en pieza, que recibe también el nombre de vuela.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img12.png')   
+                        figcaption  Nota. Tomado de https://www.shutterstock.com/es/image-photo/pink-blue-fabric-136422809 
+        .row.justify-content-center
+          .col-lg-10.tema6-div-slider.p-3
+            .tabla-a.color-acento-botones
+              table.mb-5  
+                thead
+                  tr
+                    th Nombre de la tela (tejido)
+                    th Descripción
+                    th Aspecto                  
+                tbody
+                  tr
+                    td.text-center <b>Voile</b>
+                    td Tejido de seda o rayón de poca densidad, ligamento tafetán. Se usa para vestidos o prendas de adorno. Se fabrica un tejido análogo o algún parecido con algodón peinado y, algunas veces mercerizado en pieza, que recibe también el nombre de vuela.
+                    td 
+                      figure
+                        img(src='@/assets/curso/tema7/img13.png')    
+                        figcaption Nota. Tomado de https://www.shutterstock.com/es/image-photo/iridescent-fabric-folds-background-image-1599148924
+                
 
 </template>
 
